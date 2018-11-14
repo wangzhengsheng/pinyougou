e95efc8ea25b38ca50f41f0d14ca5322ab4e2918
王正升 <769718717@qq.com>
2018-11-14 23:54:18 +0800

    品优购项目day02提交

---
+++ pinyougou-manager-web/target/pinyougou-manager-web-0.0.5-SNAPSHOT/js/service/brandService.js
@@ -0,0 +1,26 @@
+//前端代码 遵循mvc开发模式  将service与controller分离  整个后台  作为dao获取数据
+		//品牌服务层
+		app.service("brandService",function ($http){
+			//读取列表数据绑定到表单中 
+			this.findAll=function(){
+				return $http.get('../brand/findAll.do'); 
+			}
+			this.findPage=function(page,size){
+				return $http.get('../brand/findPage.do?page='+page +'&size='+size);
+			}
+			this.findOne=function(id){
+				return $http.post('../brand/findOne.do?id='+id);
+			}
+			this.add=function(entity){
+				return $http.post('../brand/add.do',entity)
+			}
+			this.update=function(entity){
+				return $http.post('../brand/update.do',entity)
+			}
+			this.dele=function(ids){
+				return $http.get('../brand/delete.do?ids='+ids)
+				}
+			this.search=function(page,rows,searchEntity){
+				return $http.post('../brand/search.do?page='+page +'&size='+rows, searchEntity)
+			}
+		});
